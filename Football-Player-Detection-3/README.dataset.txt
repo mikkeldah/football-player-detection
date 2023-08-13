cd25a5e614ebcc9dd528af4a49e80ccd76535490
Mikkel Dahl Slettebø <mikkeldahlsletteboe@gmail.com>
2023-08-13 14:51:36 +0200

    yolo comparison

---
+++ Football-Player-Detection-3/README.dataset.txt
@@ -0,0 +1,6 @@
+# Football Player Detection > 2023-08-13 2:08pm
+https://universe.roboflow.com/mikkel-ds/football-player-detection-xvszo
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
--- utils.py
+++ utils.py
@@ -3,14 +3,20 @@ import matplotlib.pyplot as plt
 import ultralytics
 import numpy as np
 
+
+class_to_name = {
+    0: "ball",
+    1: "keeper",
+    2: "player",
+    3: "referee",
+}
+
 def visualize(im_path, label_path):
     name = im_path
     im = cv2.imread(name)
     im_height, im_width, _ = im.shape
 
     labels = label_path
-
-    print(f"Image height: {im_height}, Image width: {im_width}")
 
     with open(labels, "r") as f:
         lines = f.readlines()
@@ -20,10 +26,11 @@ def visualize(im_path, label_path):
             x1, y1, x2, y2 = ultralytics.utils.ops.xywh2xyxy(np.array([x, y, w, h]))
             x1, y1, x2, y2 = int(x1*im_width), int(y1*im_height), int(x2*im_width),int(y2*im_height)
 
-            cv2.rectangle(im, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            cv2.putText(im, str(cls), (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 0, 255), 1)
+            cv2.rectangle(im, (x1, y1), (x2, y2), (204, 0, 0), 1)
+            cv2.putText(im, text=class_to_name[cls], org=(x1, y1-5), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=0.6, color=(204, 0, 0), thickness=1)
 
     img = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
     plt.figure(figsize=(12, 8), dpi=150)
     plt.imshow(img)
     plt.show()
+
